9bbf37c6f5275a50da802df0db884214a90acc75
DEVxEAZY <wylgnerbossman@gmail.com>
2026-01-12 17:43:50 -0300

    feat: adiciona exemplos de tuplas e conteúdo extra

---
+++ __basic__/src/07_tuplas.ts
@@ -0,0 +1,9 @@
+type NameList = string[]
+
+type CalendarDate = [day: number, month: number, year: number]
+
+const list: NameList = [];
+
+list.push("Will");
+
+const date: CalendarDate = [43,432,53] 
---
+++ __basic__/src/extra/desestruturacao.js
@@ -0,0 +1,15 @@
+function main(){
+
+    const person = {
+        name: "will",
+        age: 23,
+        isProgrammer:  true
+    }
+
+    const {name} = person;
+    console.log (name)
+
+}
+
+
+
